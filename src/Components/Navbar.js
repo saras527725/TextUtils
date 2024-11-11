d6c5aaefad22cd68e3d95676a2657354c61ebaf5
SARAS KUMAR <saraskumar52772525@gmail.com>
2024-11-11 13:17:25 +0530

    Updated text manipulation functions

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,25 +22,7 @@ const Navbar = (props) => {
         </li> */}
            <button className="nav-link active btn" onClick={props.navigateToAbout}>{props.about}</button>
            </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="/">{props.abc}</a></li>
-            <li><a className="dropdown-item" href="/">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="/">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true" href="/">Disabled</a>
-        </li>
       </ul>
-      <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
     {/* <div className="form-check form-switch "> */}
      <div className={`form-check form-switch text-${props.mode===`light` ?`dark`:`light`}`}> {/*in this JS use because we want text color of "Mode" word (that is wriiten below 2 line) also change after dark/light color */}
